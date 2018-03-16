20cba4ff93faf7aa8da4d99bcb0e0ee84bdd866c
Gabe Tiller <gabectiller@gmail.com>
2018-03-16 00:04:54 -0400

    automated using whenever

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,7 +2,7 @@ module ItemsHelper
 
   def time_left(item)
     from_time = Time.now - item.created_at
-    to_time = 1.days
+    to_time = 7.days
     time_left = distance_of_time_in_words(from_time, to_time )
   end
 
